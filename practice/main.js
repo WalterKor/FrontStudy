55c2d460723e4189ee51a808a2c4d69aea77a8ca
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-07 12:32:02 +0900

    7/7

---
+++ practice/main.js
@@ -0,0 +1,10 @@
+    
+    document.querySelector('.btn2').addEventListener('click', function () {
+      document.querySelector('.container').style.transform = 'translate(-100vw)';
+    })
+    document.querySelector('.btn3').addEventListener('click', function(){
+      document.querySelector('.container').style.transform = 'translate(-200vw)';
+    })
+    document.querySelector('.btn1').addEventListener('click', function(){
+      document.querySelector('.container').style.transform = 'translate(0vw)';
+    })
